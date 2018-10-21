7ee6c69389c936b93d06a7ab8fce59ea77852533
Ariful Haque <arif.fendonus@gmail.com>
2018-10-22 01:20:56 +0600

    Drawable icon can be added now also the background color can be changed

--- awesometoast/src/main/java/com/fendonus/awesometoast/AwesomeToast.java
+++ awesometoast/src/main/java/com/fendonus/awesometoast/AwesomeToast.java
@@ -2,23 +2,44 @@ package com.fendonus.awesometoast;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AwesomeToast {
-    public static void simpleToast(Context context, String message,int color){
+
+    public static void simpleToast(Context context, String message, int textColor, int bgColor){
         Toast toast =  Toast.makeText(context, message, Toast.LENGTH_SHORT);
         View toastView = toast.getView();
         TextView toastMessage = (TextView) toastView.findViewById(android.R.id.message);
         toastMessage.setText(message);
         toastMessage.setTextSize(22);
-        toastMessage.setTextColor(context.getResources().getColor(color));
+        toastMessage.setTextColor(context.getResources().getColor(textColor));
         toastMessage.setGravity(Gravity.CENTER);
         toastMessage.setCompoundDrawablePadding(16);
-        toastView.setBackgroundColor(Color.CYAN);
+        toastView.setBackgroundColor(context.getResources().getColor(bgColor));
         toast.show();
     }
+
+    public static void awesomeToast(Context context,String message, int bgColor,int textColor,int icon){
+        View v; // Creating an instance for View Object
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        v = inflater.inflate(R.layout.toast_layout, null);
+
+        TextView toastMessage = v.findViewById(R.id.toast_message);
+        ImageView imageView = v.findViewById(R.id.toast_icon);
+
+        toastMessage.setText(message);
+        toastMessage.setTextColor(context.getResources().getColor(textColor));
+        v.getBackground().setColorFilter(context.getResources().getColor(bgColor), PorterDuff.Mode.SRC_IN);
+        toastMessage.setGravity(Gravity.CENTER);
+        imageView.setImageResource(icon);
+    }
+
 
 }
